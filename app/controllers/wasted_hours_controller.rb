c6a2d347714c313b92d9d2d8c7dd839f23eda1dd
blackbaud-napoleonkernessant <napoleon.kernessant@blackbaud.com>
2014-07-30 09:49:33 -0500

    create a new page for handling wasted hours

--- app/controllers/wasted_hours_controller.rb
+++ app/controllers/wasted_hours_controller.rb
@@ -3,6 +3,9 @@ require "#{Rails.root}/lib/header/table_header"
 class WastedHoursController < ApplicationController
   def index
     header = TableHeader.new
+    @current_year = Time.new.year
+    @today_date = Time.now.strftime("%m/%d/%Y")
+    puts "----------------------- #{@today_date}"
     @table_header =  header.table_header
   end
 end
