f611ee7750f6dfeb37e2c070ef3335bd99ffa348
blackbaud-napoleonkernessant <napoleon.kernessant@blackbaud.com>
2014-07-31 14:13:56 -0500

    Time column is now working. Add a temp delete operation

--- app/controllers/wasted_hours_controller.rb
+++ app/controllers/wasted_hours_controller.rb
@@ -20,7 +20,31 @@ class WastedHoursController < ApplicationController
       lost_productivity.total_employees_affected = params[:numberOfEmployee]
       lost_productivity.hours_lost_per_employee = params[:numberOfHours]
       lost_productivity.description = params[:description]
-      lost_productivity.total_hours_lost_employees = params[:numberOfHours].to_f * 60
+      total_hours_lost = params[:numberOfEmployee].to_f * params[:numberOfHours].to_f
+      lost_productivity.total_hours_lost_employees = format_time(total_hours_lost)
     end
+    redirect_to :controller => :wasted_hours, :action => :index
+  end
+
+  def format_time(user_input)
+    output = 0
+    if user_input < 1
+      output = format_minutes(user_input)
+    else
+      temp = user_input * 60
+      hours = temp.to_i / 60
+      minutes = user_input - hours
+      output =  hours + format_minutes(minutes)
+    end
+    output
+  end
+
+  def format_minutes(user_input)
+    output = user_input * 60
+    ".#{output}".to_f
+  end
+
+  def admin_factory_reset
+    LostProductivity.delete_all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'wasted_hours#index'
   get 'wasted_hours/new'
   get 'wasted_hours/process_new_form'
+  get 'wasted_hours/admin_factory_reset'
 end
